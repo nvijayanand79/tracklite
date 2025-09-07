2f7e9435a949fe9be950d3526ee59bf032bee78a
nvijayanand79 <nvijayanand79@gmail.com>
2025-09-07 18:41:06 +0530

    Remove Python API - Convert to frontend-only application

--- api/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# This file makes the models directory a Python package
-from .base import BaseModel, UUIDMixin, TimestampMixin
-from .receipt import Receipt
-from .labtest import LabTest, LabTransfer
-from .report import Report
-from .invoice import Invoice
-from .owner import RetestRequest, OwnerPreference
